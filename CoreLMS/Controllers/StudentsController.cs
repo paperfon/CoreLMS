4aef6663b78de9abb30632dc740bb7373f4866c1
Rod Paperfon <admin@paperfon.com>
2020-02-05 16:29:10 +0100

    Injecting usermanager into students controller

--- CoreLMS/Controllers/StudentsController.cs
+++ CoreLMS/Controllers/StudentsController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreLMS.Core.Models;
 using CoreLMS.Core.ViewModels;
 using CoreLMS.Data;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,20 +15,22 @@ namespace CoreLMS.Controllers
     public class StudentsController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<LMSUser> userManager;
 
-        public StudentsController(ApplicationDbContext context)
+        public StudentsController(ApplicationDbContext context, UserManager<LMSUser> userManager)
         {
             _context = context;
+            this.userManager = userManager;
         }
 
         // GET: Students
         public async Task<IActionResult> Index()
         {
-            var model = await _context.Users
+            var model = await userManager.Users
                 .Select(s => new StudentListViewModel
                 {
-                    FirstName = s.UserName,
-                    LastName = s.UserName,
+                    FirstName = s.FirstName,
+                    LastName = s.LastName,
                     Email = s.Email
                 }).ToListAsync();
 
