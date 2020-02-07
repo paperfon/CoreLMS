94d2c13cc3bba5d895beebf89e8247cdd963d8c1
Mohammad Khattab <MOHAMMAD.KHATTAB.987@gmail.com>
2020-02-07 10:41:07 +0100

    Student Page View

--- CoreLMS/Controllers/StudentsController.cs
+++ CoreLMS/Controllers/StudentsController.cs
@@ -36,5 +36,44 @@ namespace CoreLMS.Controllers
 
             return View(model);
         }
+
+        public async Task<IActionResult> StudentPage()
+        {
+           var stu_id = userManager.GetUserId(User);
+            var course_id = await _context.LMSUserCourses
+                .Where(s => s.LMSUserId == stu_id)
+                .Select(c => c.CourseId)
+                .FirstOrDefaultAsync();
+
+            var moduleid = _context.Module
+                .Where(m => m.CourseId == course_id)
+                .Select(m=> m.ModuleId)
+                .FirstOrDefault();
+
+            var stu_activities = _context.Activity.Where(a => a.ModuleId == moduleid).ToList();
+
+            var model = from c in _context.Course
+                        .Where(c=> c.CourseId==course_id)
+                         join m in _context.Module 
+                         on c.CourseId equals m.CourseId
+                         join a in _context.Activity 
+                         on m.ModuleId equals a.ModuleId into sp
+                         from s in sp.DefaultIfEmpty()
+                         select new StudenPageViewModel 
+                         {
+                             CourseName =c.CourseName,
+                                 CourseStartDate = c.StartDate,
+                                 CourseEndDate = c.EndDate,
+                                 ModuleName = m.ModuleName,
+                                 ModuleStartDate = m.StartDate,
+                                 ModuleEndDate = m.EndDate,
+                             ActivityName= s.ActivityName,
+                             ActivityStartDate=s.StartDate,
+                             ActivityEndDate=s.EndDate,
+                             ActivityType=s.ActivityType.ToString()
+                         }; 
+
+            return View(model);
+        }
     }
 }
---
+++ CoreLMS/Core/ViewModels/StudenPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreLMS.Core.ViewModels
+{
+    public class StudenPageViewModel
+    {
+     
+        public string CourseName { get; set; }
+        public DateTime CourseStartDate { get; set; }
+        public DateTime CourseEndDate { get; set; }
+
+        public string ModuleName { get; set; }
+        public DateTime ModuleStartDate { get; set; }
+        public DateTime ModuleEndDate { get; set; }
+
+        public string ActivityName { get; set; }
+        public DateTime ActivityStartDate { get; set; }
+        public DateTime ActivityEndDate { get; set; }
+
+        public string ActivityType { get; set; }
+
+       
+    }
+}
