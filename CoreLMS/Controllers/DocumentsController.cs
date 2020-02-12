fe8cd4b2148056e822899bb72c6ef0caec533e7e
Lakshmi kalavathi <56877693+lakshmikalaambati@users.noreply.github.com>
2020-02-12 12:04:46 +0100

    Added Download link to Documents

--- CoreLMS/Controllers/DocumentsController.cs
+++ CoreLMS/Controllers/DocumentsController.cs
@@ -126,6 +126,52 @@ namespace CoreLMS.Controllers
             return View();
         }
 
+        
+        public async Task<IActionResult> Download(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var filepath = _context.Documents.Where(d => d.DocumentId == id).Select(d => d.DocumentPath).FirstOrDefault();
+
+            var memory = new MemoryStream();
+            using (var stream = new FileStream(filepath, FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+            return File(memory, GetContentType(filepath), Path.GetFileName(filepath));
+        }
+
+        private string GetContentType(string path)
+        {
+            var types = GetMimeTypes();
+            var ext = Path.GetExtension(path).ToLowerInvariant();
+            return types[ext];
+        }
+
+        private Dictionary<string, string> GetMimeTypes()
+        {
+            return new Dictionary<string, string>
+            {
+                {".txt", "text/plain"},
+                {".pdf", "application/pdf"},
+                {".doc", "application/vnd.ms-word"},
+                {".docx", "application/vnd.ms-word"},
+                {".xls", "application/vnd.ms-excel"},
+                {".xlsx", "application/vnd.openxmlformats,officedocument.spreadsheetml.sheet"},
+                {".png", "image/png"},
+                {".jpg", "image/jpeg"},
+                {".jpeg", "image/jpeg"},
+                {".gif", "image/gif"},
+                {".csv", "text/csv"}
+            };
+        }
+
+
+
         [Route("Create/{id}/{name}")]
         [HttpPost]
         public async Task<IActionResult> CreateAsync(UploadFile model, int id, string name)
@@ -160,7 +206,13 @@ namespace CoreLMS.Controllers
                 var uploadsFolder = Path.Combine(projectDir, "wwwroot/Dox");
                 FileName = Path.GetFileName(model.File.FileName);
                 filePath = Path.Combine(uploadsFolder, FileName);
-                model.File.CopyTo(new FileStream(filePath, FileMode.Create));
+                //model.File.CopyTo(new FileStream(filePath, FileMode.Create));
+                using (FileStream f = new FileStream(filePath, FileMode.Create))
+                {
+                    model.File.CopyTo(f);
+                    f.Close();
+                }
+
 
             }
 
