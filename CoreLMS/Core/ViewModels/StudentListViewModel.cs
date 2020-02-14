284c7c9e9f5b8d9de4274d4db39584bdce186703
Rod Paperfon <admin@paperfon.com>
2020-02-14 10:48:17 +0100

    Student details view and controller

--- CoreLMS/Core/ViewModels/StudentListViewModel.cs
+++ CoreLMS/Core/ViewModels/StudentListViewModel.cs
@@ -9,6 +9,7 @@ namespace CoreLMS.Core.ViewModels
 {
     public class StudentListViewModel
     {
+        public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
